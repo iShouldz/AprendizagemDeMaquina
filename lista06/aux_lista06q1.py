af8b4549719752f908743455c94443ba6e2733f3
Pedro Souza <pedrosport10@gmail.com>
2023-08-01 21:35:59 -0300

    Arquivo para tratamento da qs1

---
+++ lista06/aux_lista06q1.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+baseSplitados = []
+
+base = open('IrisSaida.csv', 'r')
+linhas = base.readlines()
+del linhas[0]
+
+for i in linhas:
+    teste = i.split(',')
+    baseSplitados.append(teste)
+
+for i in range(len(baseSplitados)):
+    del baseSplitados[i][0]
+
+baseNp = np.array(baseSplitados)
+
+def separarX(data):
+    arrayX = []
+    for i in range(len(data)):
+        arrayX.append(np.delete(data[i], [4]))
+    arrayXnp = np.array(arrayX)
+    return arrayXnp
+
+def separarY(data):
+    arrayY = []
+    for i in range(len(data)):
+        arrayY.append(np.delete(data[i], [0, 1, 2, 3]))
+    arrayYnp = np.array(arrayY)
+    return arrayYnp
+
+irisX = separarX(baseNp)
+irisY = separarY(baseNp)
+irisY = irisY.ravel()
+
+def irisTreinoTeste():
+    x_treino, x_teste, y_treino, y_teste = train_test_split(irisX, irisY, test_size=0.5)
+    return x_treino, x_teste, y_treino, y_teste
+print(baseSplitados)
+
+
+
